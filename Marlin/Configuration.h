9a894adb02a3bdfb89ec2f905f88a6a47464ec26
Jeremy D. Russell <what_snew@hotmail.com>
2018-06-21 10:27:41 -0400

    FilOut working
    Todo - reduce post-change extrusion length
    	- bed level change order to front left first

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -838,7 +838,9 @@
  * For other boards you may need to define FIL_RUNOUT_PIN.
  * By default the firmware assumes HIGH = has filament, LOW = ran out
  */
-//#define FILAMENT_RUNOUT_SENSOR
+ // JDR - note that the below define needed an edit in pin_RAMPS.h to avoid it being overwritten
+#define FIL_RUNOUT_PIN 2 
+#define FILAMENT_RUNOUT_SENSOR
 #if ENABLED(FILAMENT_RUNOUT_SENSOR)
   #define FIL_RUNOUT_INVERTING false // set to true to invert the logic of the sensor.
   #define ENDSTOPPULLUP_FIL_RUNOUT // Uncomment to use internal pullup for filament runout pins if the sensor is defined.
@@ -1179,7 +1181,8 @@
  *    P1  Raise the nozzle always to Z-park height.
  *    P2  Raise the nozzle by Z-park amount, limited to Z_MAX_POS.
  */
-//#define NOZZLE_PARK_FEATURE
+// JDR
+#define NOZZLE_PARK_FEATURE
 
 #if ENABLED(NOZZLE_PARK_FEATURE)
   // Specify a park position as { X, Y, Z }
